107dced5b65e562a16e11c8d324939ba6f4c5cb6
bhadurianirban <bhaduri.anirban@gmail.com>
2019-02-13 21:02:52 +0530

    home

---
+++ src/main/java/org/dgrf/cloud/client/DGRFCloudConstants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dgrf.cloud.client;
+
+/**
+ *
+ * @author bhaduri
+ */
+public class DGRFCloudConstants {
+    public static final String BASE_URL = "http://localhost:8080/DGRFCloud/rest";
+    //public static final String BASE_URL = "http://35.197.31.87:8080/DGRFCloud/rest";
+}
